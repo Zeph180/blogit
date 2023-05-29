4e8ad83bb30152e4ed01df069807f6583b9bba34
Samuel Amankwaa-Frempong <samankwaa.frempong@gmail.com>
2023-05-29 09:47:00 +0000

    Add rails require for rspec rails

--- spec/views/post_index_spec.rb
+++ spec/views/post_index_spec.rb
@@ -1,3 +1,4 @@
+require 'rails_helper'
 RSpec.describe 'Render posts index page', type: :feature do
   before :each do
     @user = User.create(name: 'Lillian', photo: 'https://media.gettyimages.com/id/1278139568/nl/foto/studioportret-van-20-jaar-oude-vrouw.jpg?s=612x612&w=0&k=20&c=3Bd4Ot79Z1ZKoCwAl0qFQ9hoBrQTar4SqtPefHOBEkg=',
@@ -10,3 +11,4 @@ RSpec.describe 'Render posts index page', type: :feature do
     Comment.create(text: 'You know Rails', author_id: @user.id, post_id: @first_post.id)
     visit user_posts_path(@user)
   end
+end
