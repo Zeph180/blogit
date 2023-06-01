7b27d0dc35ab4609da3416867a5c158e53922104
Zeph180 <zephrichards1@gmail.com>
2023-06-01 22:50:41 +0300

    Modify comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,5 @@
 class CommentsController < ApplicationController
   load_and_authorize_resource
-  def index
-    @posts = Post.find(params[:post_id])
-    @user = User.find(params[:user_id])
-    @comments = Comment.find(params[:comment_id])
-
-    respond_to do |format|
-      format.html
-      format.json { render json: @comments }
-    end
-  end
-
   def new
     @user = User.find(params[:user_id])
     @post = @user.posts.find(params[:post_id])
@@ -19,20 +8,13 @@ class CommentsController < ApplicationController
   def create
     @user = User.find(params[:user_id])
     @post = Post.find(params[:post_id])
-    @post = Post.find(params[:post_id])
 
     @comment = @post.comments.new(comment_params)
     @comment.author = @user
-    respond_to do |format|
-      if @comment.save
-        format.html { redirect_to request.referrer, notice: 'comment was successfully created.' }
-        format.json { render Json: @comment, status: :created }
-      else
-        format.html { render :new }
-        format.json do
-          render json: @comment, status: :created
-        end
-      end
+    if @comment.save
+      redirect_to request.referrer
+    else
+      render :new
     end
   end
 
