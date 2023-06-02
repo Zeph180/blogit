ef52ffc3634776b63d1b5bf8e328708b4ecc3820
Samuel Amankwaa-Frempong <samankwaa.frempong@gmail.com>
2023-06-02 15:15:01 +0000

    Add most_recent_post method to user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
-  
   has_many :posts, foreign_key: 'author_id', dependent: :destroy
   has_many :comments, foreign_key: 'author_id', dependent: :destroy
   has_many :likes, foreign_key: 'author_id', dependent: :destroy
   
   validates :name, presence: true
+  
+  def most_recent_posts
+    Post.limit(3).order(created_at: :desc).where(author: self)
+  end
 end
