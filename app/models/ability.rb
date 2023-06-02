38b076b37c434acd0d0dece9ca7b70f06e9b9788
Racheal Appiah-kubi <coderacheal@gmail.com>
2023-06-02 15:23:01 +0000

    Add initialize method

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,6 +1,15 @@
 class Ability
   include CanCan::Ability
-  
+  def initialize(user)
+    return unless user.present?
+    can :read, :all
+    can :create, [Comment, Post]
+    if user.role == 'admin'
+      can :manage, :all
+    else
+      can :destroy, Comment, author: user
+      can :destroy, Post, author: user
+    end
     # Define abilities for the user here. For example:
     #
     #   return unless user.present?
