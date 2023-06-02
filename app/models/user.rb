537451911355a1f737a6493923594eeb66d45c99
Samuel Amankwaa-Frempong <samankwaa.frempong@gmail.com>
2023-06-02 15:13:45 +0000

    Add validation to user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,6 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: 'author_id', dependent: :destroy
   has_many :comments, foreign_key: 'author_id', dependent: :destroy
   has_many :likes, foreign_key: 'author_id', dependent: :destroy
+  
+  validates :name, presence: true
 end
