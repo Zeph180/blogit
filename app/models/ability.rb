5caf5e6ab0577e7caf4bd0d764960c9eddbf9a21
Racheal Appiah-kubi <coderacheal@gmail.com>
2023-06-02 07:04:23 +0000

    Add Ability class

---
+++ app/models/ability.rb
@@ -0,0 +1,18 @@
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    return unless user.present?
+
+    can :read, :all
+    can :create, [Comment, Post]
+
+    if user.admin?
+      can :manage, :all
+    else
+      can :destroy, Comment, author: user
+      can :destroy, Post, author: user
+    end
+    
+  end
+end
