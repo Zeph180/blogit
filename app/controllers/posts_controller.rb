3432157683508ef091cff587da5fad63d306c85a
Zeph180 <zephrichards1@gmail.com>
2023-06-03 19:41:31 +0300

    Add endpoint

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,24 @@
 class PostsController < ApplicationController
   load_and_authorize_resource
-  skip_before_action :verify_authenticity_token, only: %i[destroy create]
-
+  protect_from_forgery prepend: true
   def index
     @user = User.find(params[:user_id])
     @posts = @user.posts.includes(:comments)
+    respond_to do |format|
+      format.html
+      format.xml { render xml: @posts }
+      format.json { render json: @posts, status: :ok }
+    end
   end
 
   def show
     @post = Post.find(params[:id])
     @user = User.find(@post.author_id)
+    respond_to do |format|
+      format.html
+      format.xml { render xml: @posts.comments }
+      format.json { render json: @posts.comments }
+    end
   end
 
   def new
